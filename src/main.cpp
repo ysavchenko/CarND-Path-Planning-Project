1ad7c779e847990b524426d794868e93665aea70
Yuriy Savchenko <yuriy.savchenko@gmail.com>
2018-03-10 19:23:15 +0200

    Added working path planning algorithm and report

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -163,6 +164,83 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s, const vec
 
 }
 
+#define DISTANCE_SAFE_FRONT   15
+#define DISTANCE_SAFE_BACK    5
+#define POINT_INTERVAL        0.02
+#define LANE_MIN    0
+#define LANE_MAX    2
+#define SPEED_MAX   49.5
+#define SPEED_STEP  0.224
+#define MPH_TO_MS   2.24
+#define FUTURE_DISTANCE   50
+#define TARGET_DISTANCE   30
+#define POINTS_COUNT      50
+
+// Class Lane encapsulates lane states (are they safe to change into and what is the safe speed there)
+class Lane {
+
+public:
+  int index;          // Lane index (0, 1, 2)
+  double interval;    // Planning interval (seconds)
+  double d_from, d_to;  // Lane d coordinates (from, to)
+  double car_s, car_speed;  // Our vehicle s and speed
+  bool is_safe;
+  double safe_speed;
+
+  Lane(int index) {
+    this->index = index;
+    d_from = index * 4;
+    d_to = (index + 1) * 4;
+    is_safe = true;
+  }
+
+  // Returns the safe speed on this lane
+  double getSafeSpeed() {
+    return is_safe? safe_speed: 0;
+  }
+
+  // Returns the center of the lane
+  double getCenter() {
+    return (d_from + d_to) / 2;
+  }
+
+  // Inits the lane with current car position
+  void setCarPosition(double s, double speed, double interval) {
+    this->interval = interval;
+    car_s = s;
+    car_speed = speed;
+    safe_speed = car_speed;   // By default we assume that current speed is safe
+  }
+
+  // Updates lane safety status with each vehicle on the road
+  void updateStatus(vector<double> vehicle_info) {
+    if (!is_safe)
+      return;     // Lane already unsafe
+    double d = vehicle_info[6];
+    if (d < d_from || d > d_to)
+      return;   // Vehicle is not in this lane
+
+    double vx = vehicle_info[3];
+    double vy = vehicle_info[4];
+    double speed = sqrt(vx * vx + vy * vy);
+    double s = vehicle_info[5];
+
+    double distance_now = s - car_s;
+    double distance_future = distance_now + (speed - car_speed) * interval;
+    if (distance_now > DISTANCE_SAFE_FRONT && distance_future > DISTANCE_SAFE_FRONT)
+      return; // The car is far away in front of our car, ignore it
+    if (distance_now < -DISTANCE_SAFE_BACK && distance_future < -DISTANCE_SAFE_BACK)
+      return; // The car is behind us, we can ignore it
+    if (distance_now > 0) {
+      double new_safe_speed = speed - (DISTANCE_SAFE_FRONT - distance_now) / interval;
+      if (safe_speed > new_safe_speed)
+        safe_speed = new_safe_speed;       // If car is in front of us -- adjust the safe speed
+    } else {
+      is_safe = false;
+    }
+  }
+};
+
 int main() {
   uWS::Hub h;
 
@@ -200,7 +278,12 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  //start in lane 1(middle), valid values = {0,1,2}
+	int lane = 1;
+	double ref_vel = 0.0; //(mph) start at 0 and accelerate slowly
+	int lane_change_cnt = 0; //keep track of the number of cycles since the last lane change
+
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane, &ref_vel, &lane_change_cnt](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -242,9 +325,130 @@ int main() {
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
+            int prev_size = previous_path_x.size();	
+            double interval = prev_size * POINT_INTERVAL;
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
-          	msgJson["next_x"] = next_x_vals;
+            double target_speed = min(ref_vel + SPEED_STEP, SPEED_MAX);
+
+            vector<Lane> lanes;
+            for (int index = LANE_MIN; index <= LANE_MAX; index++) {
+              Lane lane_info = Lane(index);
+              lane_info.setCarPosition(car_s, target_speed, interval);
+              lanes.push_back(lane_info);
+            }
+
+            // Update lane info with vehicles
+            for (int i = 0; i < sensor_fusion.size(); i++) {
+              const vector<double>& vehicle_info = sensor_fusion[i];
+              for (Lane& lane_info : lanes) {
+                lane_info.updateStatus(vehicle_info);
+              }
+            }
+
+            Lane& current_lane = lanes[lane];
+            if (current_lane.safe_speed < target_speed) {
+              // If current lane conditions prevent us from reaching target speed try to change the lane
+              double speed_left = 0;
+              double speed_right = 0;
+              if (lane > LANE_MIN) {
+                speed_left = lanes[lane - 1].getSafeSpeed();
+              }
+              if (lane < LANE_MAX) {
+                speed_right = lanes[lane + 1].getSafeSpeed();
+              }
+              if (speed_left >= speed_right && speed_left > current_lane.safe_speed) {
+                lane--;
+              } else if (speed_right >= speed_left && speed_right > current_lane.safe_speed) {
+                lane++;
+              }
+              //std::cout << "Lane speeds " << lanes[0].getSafeSpeed() << ", " << lanes[1].getSafeSpeed() << ", " << lanes[2].getSafeSpeed() << std::endl;
+            }
+            // Update target velocity using current lane's target speed
+            current_lane = lanes[lane];
+            if (ref_vel > current_lane.safe_speed)
+              ref_vel = max(ref_vel - SPEED_STEP, current_lane.safe_speed);
+            else
+              ref_vel = min(ref_vel + SPEED_STEP, current_lane.safe_speed);
+            
+            // Prepare sparse path points to move spline through them later    
+            vector<double> ptsx;
+            vector<double> ptsy;
+
+            double ref_x = car_x;
+            double ref_y = car_y;
+            double ref_yaw = deg2rad(car_yaw);
+
+            if (prev_size < 2) {
+              // Estimate previous position from current
+              double prev_car_x = car_x - cos(ref_yaw);
+              double prev_car_y = car_y - sin(ref_yaw);
+              ptsx.push_back(prev_car_x);
+              ptsx.push_back(car_x);
+              ptsy.push_back(prev_car_y);
+              ptsy.push_back(car_y);
+            } else {
+              // Use actual previous position
+              ref_x = previous_path_x[prev_size - 1];
+              ref_y = previous_path_y[prev_size - 1];
+                
+              double ref_x_prev = previous_path_x[prev_size - 2];
+              double ref_y_prev = previous_path_y[prev_size - 2];
+              ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+              ptsx.push_back(ref_x_prev);
+              ptsx.push_back(ref_x);
+              ptsy.push_back(ref_y_prev);
+              ptsy.push_back(ref_y);	
+            }
+
+            // Add our target points
+            for (int future_step = 1; future_step <= 2; i++) {
+              vector<double> next_point = getXY(car_s + future_step * FUTURE_DISTANCE, lanes[lane].getCenter(), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+              ptsx.push_back(next_point[0]);
+              ptsy.push_back(next_point[1]);	
+            }
+			
+            // Transform points into car coordinates
+            for (int i = 0; i < ptsx.size(); i++) {
+              double shift_x = ptsx[i] - ref_x;
+              double shift_y = ptsy[i] - ref_y;
+              ptsx[i] = (shift_x * cos(-ref_yaw) - shift_y * sin(-ref_yaw));
+              ptsy[i] = (shift_x * sin(-ref_yaw) + shift_y * cos(-ref_yaw));				
+            }
+
+            // Create spline curve over the main points
+            tk::spline s;
+            s.set_points(ptsx, ptsy);
+
+            // Re-use old path
+            for (int i = 0; i < previous_path_x.size(); i++) {
+              next_x_vals.push_back(previous_path_x[i]);
+              next_y_vals.push_back(previous_path_y[i]);				
+            }
+
+            double target_x = TARGET_DISTANCE;
+            double target_y = s(target_x);
+            double target_dist = sqrt(target_x*target_x+target_y*target_y);
+            double x_add_on = 0;
+	          // Use spline to fill out the rest of the POINTS_COUNT points
+            for (int i = previous_path_x.size(); i < POINTS_COUNT; i++) {
+              double N = (target_dist / (POINT_INTERVAL * ref_vel / MPH_TO_MS));
+              double x_point = x_add_on + (target_x) / N;
+              double y_point = s(x_point);
+              x_add_on = x_point;
+
+              double x_ref = x_point;
+              double y_ref = y_point;
+
+              // Transform coordinates from car to global system
+              x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw)) + ref_x;
+              y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw)) + ref_y;
+
+              next_x_vals.push_back(x_point);
+              next_y_vals.push_back(y_point);
+            }
+            
+            msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
           	auto msg = "42[\"control\","+ msgJson.dump()+"]";
